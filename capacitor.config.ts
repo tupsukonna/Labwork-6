53c80cec3f4d48b585583e4c9673fcec5db5042c
TupsuKonna <46424503+tupsukonna@users.noreply.github.com>
2022-05-02 08:23:58 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PenPals',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
